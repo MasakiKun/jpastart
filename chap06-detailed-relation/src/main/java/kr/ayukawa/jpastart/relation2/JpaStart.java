ffe5d28de9a314cea0678c63972335425e88cb0c
MasakiKun <kidconan@gmail.com>
2019-03-15 14:42:42 +0900

    EntityManagerFactory 삭제

--- chap06-detailed-relation/src/main/java/kr/ayukawa/jpastart/relation2/JpaStart.java
+++ chap06-detailed-relation/src/main/java/kr/ayukawa/jpastart/relation2/JpaStart.java
@@ -4,8 +4,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JpaStart {
-	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("relation2");
-
 	public static void main(String[] args) {
 
 	}
